88a6d1a799a63ba4906dbaa871a89843e87243e0
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-03-04 16:51:30 +0500

    Add welocome in the navbar

--- social_app/users/views.py
+++ social_app/users/views.py
@@ -30,7 +30,8 @@ def index(request):
     current_user = request.user
     posts = Post.objects.filter(user=current_user)
     profile = Profile.objects.filter(user=current_user).first
-    return render(request,'users/index.html',{'posts':posts, 'profile': profile})
+    logged_user = request.user
+    return render(request,'users/index.html',{'posts':posts, 'profile': profile,'logged_user':logged_user})
 
 
 def register(request):
