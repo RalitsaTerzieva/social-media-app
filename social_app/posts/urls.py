2d9a7f3bc84c6f5691692590389cb81b284d1a61
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-03-11 11:17:41 +0500

    Add like view and url

--- social_app/posts/urls.py
+++ social_app/posts/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('create/',views.post_create,name='create'),
     path('feed/',views.feed,name='feed'),
+    path('like/',views.like_post,name='like'),
 ]
--- social_app/posts/views.py
+++ social_app/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .forms import PostCreateForm
 from django.contrib.auth.decorators import login_required
 from .models import Post
@@ -21,3 +21,12 @@ def feed(request):
     posts = Post.objects.all()
     
     return render(request,'posts/feed.html',{'posts':posts})
+
+
+def like_post(request):
+    post_id = request.POST.get('post_id')
+    post = get_object_or_404(Post,id=post_id)
+    if post.liked_by.filter(id=request.user.id).exists():
+        post.liked_by.remove(request.user)
+    else:
+        post.liked_by.add(request.user)
