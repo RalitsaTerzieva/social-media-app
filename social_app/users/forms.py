9f2087d5c4d5762a411841d349edd07ea6127911
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-03-23 16:19:56 +0500

    Add register form

--- social_app/users/forms.py
+++ social_app/users/forms.py
@@ -1,5 +1,19 @@
 from django import forms
+from django.contrib.auth import User
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=150)
     password = forms.CharField(max_length=100, widget=forms.PasswordInput)
+    
+    
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password',widget=forms.PasswordInput)
+    password1 = forms.CharField(label='Confirm password',widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ('username','email','first_name')
+        
+    def check_password(self):
+        if self.cleaned_data['password'] != self.cleaned_data['password2']:
+            raise forms.ValidationError('Passwords do not match!')
+        return self.cleaned_data['password2']
