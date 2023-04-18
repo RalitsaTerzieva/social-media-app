3593a14053a1ff2b65a9ad9fc3e3b6f5fe3a10b0
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-18 12:23:38 +0500

    Add logout logic

--- social_app/users/urls.py
+++ social_app/users/urls.py
@@ -1,6 +1,8 @@
 from users import views
 from django.urls import path
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path("login/",views.user_login,name='login'),
+    path("logout/",auth_view.LogoutView.as_view(template_name='users/logout.html'),name="logout"),
 ]
