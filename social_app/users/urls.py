27db4c23eebf1d91c6abc29f393a6e3991488c9b
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-18 14:07:45 +0500

    Add index html and protect route

--- social_app/users/urls.py
+++ social_app/users/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_view
 
 urlpatterns = [
+    path("",views.index,name="index"),
     path("login/",views.user_login,name='login'),
     path("logout/",auth_view.LogoutView.as_view(template_name='users/logout.html'),name="logout"),
 ]
--- social_app/users/views.py
+++ social_app/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -19,3 +20,7 @@ def user_login(request):
     else:    
         form = LoginForm()
     return render(request, 'users/login.html', {'form': form})
+
+@login_required
+def index(request):
+    return render(request,'users/index.html')
