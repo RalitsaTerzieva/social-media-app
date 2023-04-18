dc2ecad0f254eb5a2d171a774f4bfd7a8c7466f6
Ralitsa Terzieva <ralicaterzieva90@gmail.com>
2023-04-18 14:34:18 +0500

    Add PasswordChange logic

--- social_app/users/urls.py
+++ social_app/users/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("",views.index,name="index"),
     path("login/",views.user_login,name='login'),
     path("logout/",auth_view.LogoutView.as_view(template_name='users/logout.html'),name="logout"),
+    path("password_change/",auth_view.PasswordChangeView.as_view(template_name='users/password_change_form.html'),name="password_change"),
+    path("password_change/done/",auth_view.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),name="password_change_done"),
 ]
